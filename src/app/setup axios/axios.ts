521d1432957629d141aca2bbe27e2d0e456f470b
alisonBRs <97985844+alisonBRs@users.noreply.github.com>
2025-04-05 20:06:25 -0300

    refact: remove token log

--- src/app/setup axios/axios.ts
+++ src/app/setup axios/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 const token = localStorage.getItem("token");
 
-console.log("token", token);
 export const fetchAxios = axios.create({
   baseURL: "http://localhost:3080",
   headers: { Authorization: token },
