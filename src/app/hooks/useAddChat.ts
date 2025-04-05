baea6101cdde96018e6571acf0067af27a5babe4
alisonBRs <97985844+alisonBRs@users.noreply.github.com>
2025-04-05 20:20:20 -0300

    fix:  add chat

--- src/app/hooks/useAddChat.ts
+++ src/app/hooks/useAddChat.ts
@@ -14,24 +14,18 @@ export function useAddChat() {
     mutationKey: ["create-chat"],
     mutationFn: addChat,
     onMutate: () => {
-      queryClient.setQueryData(["chats"], (prev: any) => {
+      queryClient.setQueryData(["profile"], (prev: any) => {
         const newChat = {
           id: `fakeId-${v4()}`,
-          message: [],
           userId: `fakeUserId-${v4()}`,
-          name: `Chat #${prev.length + 1}`,
+          name: `Chat #${prev.chats.length + 1}`,
         };
-        if (prev) {
-          return [...prev, newChat];
-        }
-
-        return [newChat];
+        return { ...prev, chats: [...prev.chats, { ...newChat }] };
       });
     },
     onSuccess: ({ data }) => {
-      console.log(data, "data");
-      queryClient.setQueryData(["chats"], (prev: any) => {
-        const updatedChat = prev.map((chat: any) => {
+      queryClient.setQueryData(["profile"], (prev: any) => {
+        const updatedChat = prev.chats.map((chat: any) => {
           if (
             chat.id.startsWith("fakeId") ||
             chat.userId.startsWith("fakeUserId")
@@ -40,13 +34,12 @@ export function useAddChat() {
               ...chat,
               id: data.result.id,
               userId: data.result.userId,
-              message: [],
             };
           }
           return chat;
         });
 
-        return updatedChat;
+        return { ...prev, chats: updatedChat };
       });
     },
     onError: () => {
